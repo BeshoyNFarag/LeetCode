65fd5d3694f79d62df34b4a4f36a3c1d4766dfd4
Beshoy Farag <beshoynady401@gmail.com>
2026-05-06 13:05:31 +0200

    add practice for some leetcode

---
+++ java beginner/src/RunningSomeOne.java
@@ -0,0 +1,16 @@
+public class RunningSomeOne {
+    public int[] runningSum(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        int temp = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            temp += nums[i];
+            result[i] = temp;
+
+        }
+
+        return result;
+    }
+}
