3d57715dbdbe55139526cb6e2cd510ee30c07066
Beshoy Farag <beshoynady401@gmail.com>
2026-05-22 17:28:47 +0200

    finish the reverse string

--- java beginner/src/ReverseWord.java
+++ java beginner/src/ReverseWord.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ReverseWord {
 
     public String reverseWords(String s) {
+        ArrayList<String> str = new ArrayList<>();
+        String result = "";
+        Collections.addAll(str, s.split(" "));
 
-        StringBuffer sb = new StringBuffer(s);
+        for (int i = 0; i < str.size(); i++) {
+            StringBuffer sb = new StringBuffer(str.get(i));
+            if (i== str.size() - 1)
+                result += sb.reverse();
 
-        String newS = sb.reverse().toString();
+            else
+                result += sb.reverse()+ " ";
 
-        return newS;
+
+        }
+//        for(String word : str){
+//            StringBuffer sb = new StringBuffer(word);
+//            result = result.concat(sb.reverse().toString()) ;
+//        }
+
+        return result;
     }
 }
